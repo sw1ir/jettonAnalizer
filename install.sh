92e08c23f348960c234635b30fdb4af2c6fa46f2
sw1ir <vasac2281337@mail.ru>
2025-07-02 11:33:08 +0300

    теперь token.html полность асинхронный изменено:      README.md

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ wget https://go.dev/dl/go1.24.4.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.24.4.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 source ~/.bashrc
-rm go1.24.4.linux-armv6l.tar.gz
+rm go1.24.4.linux-amd64.tar.gz
 
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
