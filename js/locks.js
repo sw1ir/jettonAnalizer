ebcd2aa0cb0aee8910e52a932729e77187be246f
sw1ir <vasac2281337@mail.ru>
2025-09-08 00:19:59 +0300

    modified:   js/locks.js

--- js/locks.js
+++ js/locks.js
@@ -41,9 +41,18 @@ let logHolderPercentage = (element, index, totalSupply, decimals = 9) => {
         let lock_data = document.createElement("span");
         let utime = document.createElement("span");
         
-        unlock_data.textContent = udata;
-        lock_data.textContent = ldata;
-        
+         const formatDateTime = (date) => {
+            let day = date.getDate().toString().padStart(2, '0');
+            let month = (date.getMonth() + 1).toString().padStart(2, '0');
+            let year = date.getFullYear();
+            let hours = date.getHours().toString().padStart(2, '0');
+            let minutes = date.getMinutes().toString().padStart(2, '0');
+            return `${day}.${month}.${year} ${hours}:${minutes}`;
+        };
+      
+      
+        unlock_data.textContent = formatDateTime(udata);
+        lock_data.textContent = formatDateTime(ldata);
         // Функция для обновления обратного отсчета
         function updateCountdown() {
             const now = Math.floor(Date.now() / 1000);
